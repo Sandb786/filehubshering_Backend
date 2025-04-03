3090c1fd28e3124b532bd9ff35da07415a17706b
Sandeep Mahawat <sandeepmahawat09@gmail.com>
2025-04-03 10:48:50 +0530

    Stop shceduling because time mismatched.

--- src/main/java/com/example/filehubshering/FilehubsheringApplication.java
+++ src/main/java/com/example/filehubshering/FilehubsheringApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling
 public class FilehubsheringApplication 
 {
 
